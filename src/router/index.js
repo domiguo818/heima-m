8fe15cec99a1f917dec15d86caa48267ef4e587d
domiguo818 <1193224158@qq.com>
2022-07-15 17:58:47 +0800

    下班

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,20 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [{ path: '/login', component: () => import('@/views/Login') }]
+const routes = [
+  { path: '/login', component: () => import('@/views/Login') },
+  {
+    path: '/',
+    redirect: '/ ',
+    component: () => import('@/views/Layout'),
+    children: [
+      { path: ' ', component: () => import('@/views/Home') },
+      { path: 'profile', component: () => import('@/views/My') },
+      { path: 'qa', component: () => import('@/views/QA') },
+      { path: 'video', component: () => import('@/views/Video') }
+    ]
+  }
+]
 
 const router = new VueRouter({
   routes
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    布局页面
+    <router-view></router-view>
+    <van-tabbar v-model="active" route>
+      <van-tabbar-item icon="home-o" to='/ '>
+        <template #icon>
+          <span class="toutiao toutiao-shouye"></span>
+        </template>
+        首页</van-tabbar-item
+      >
+      <van-tabbar-item icon="search" to='/video'>
+        <template #icon>
+          <span class="toutiao toutiao-shipin"></span> </template
+        >视频</van-tabbar-item
+      >
+      <van-tabbar-item icon="friends-o" to='/qa'>
+        <template #icon>
+          <span class="toutiao toutiao-wenda"></span> </template
+        >问答</van-tabbar-item
+      >
+      <van-tabbar-item icon="setting-o" to='/profile'>
+        <template #icon>
+          <span class="toutiao toutiao-wode"></span> </template
+        >我的</van-tabbar-item
+      >
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      active: 0
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'>
+.toutiao{
+  font-size: 0.5rem;
+}
+</style>
